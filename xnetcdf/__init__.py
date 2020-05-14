b602f718407abddc28b5328fc1abf7cf28fd4f1f
crvernon <chrisrvernon@gmail.com>
2020-05-14 11:51:13 -0400

    raise class to user

---
+++ xnetcdf/__init__.py
@@ -0,0 +1,10 @@
+"""High level package initialization to make classes and functions within the package visible to the user.
+
+License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
+
+"""
+
+from xnetcdf.convert import DataToArray
+
+
+__all__ = ['DataToArray']

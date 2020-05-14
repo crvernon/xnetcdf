b3709f1f7f11d1662a383a160b046753a32e2d8a
crvernon <chrisrvernon@gmail.com>
2020-05-14 11:50:38 -0400

    create data array method

--- xnetcdf/convert.py
+++ xnetcdf/convert.py
@@ -4,11 +4,15 @@ License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
 
 """
 
+import os
+
 import numpy as np
+import pandas as pd
 
 
-class XanthosToNetcdf:
-    """Convert Xanthos outputs from CSV to NetCDF files.
+class DataToArray:
+    """Convert Xanthos outputs from CSV to a 3D NumPy array having a data value for each grid cell in the global
+    coordinate plane per time step.
 
     INSTANCE VARIABLES:
 
@@ -17,12 +21,12 @@ class XanthosToNetcdf:
                                         contains headers:  'grid_id', 'latitude_index', and 'longitude_index'.
     :type xanthos_reference_file:       str
 
-    :param xanthos_output_csv:          Full path with file name and extension to a Xanthos output containing data
+    :param xanthos_data_csv:            Full path with file name and extension to a Xanthos output containing data
                                         associated with each 67420 land cell.  Data should be in the format of a row
                                         per grid cell where the columns are the values per month or year.  Contains
                                         headers:  'id', 'YYYY' or 'YYYYMM'; where 'YYYY' is a string year (e.g., '1995')
                                         and 'YYYYMM' is a string year and month (e.g., '199501').
-    :type xanthos_reference_file:       str
+    :type xanthos_data_csv:              str
 
 
     CLASS VARIABLES:
@@ -45,6 +49,33 @@ class XanthosToNetcdf:
     :param NODATA:                      NoData value of the array for non-land elements; default `np.nan`
     :type constant_value:               float; int; NaN
 
+    :param REF_COLUMNS:                 Column header names used to extract fields from the reference data
+    :type REF_COLUMNS:                  list
+
+    :param KEY:                         Primary to set that identifies the grid cell id from the Xanthos data;
+                                        default: `grid_id`
+    :type KEY:                          str
+
+    :param DATA_ID_FIELD:               Primary to set that identifies the grid cell id from the Xanthos data;
+                                        default: `grid_id`
+    :type DATA_ID_FIELD:                str
+
+    :param REF_X_FIELD:                 Reference data field name for the X index; default: `longitude_index`
+    :type REF_X_FIELD:                  str
+
+    :param REF_Y_FIELD:                 Reference data field name for the X index; default: `latitude_index`
+    :type REF_Y_FIELD:                  str
+
+
+    Examples:
+        # Option 1:  run model for all years by passing a configuration YAML as the sole argument
+        >>> from xnetcdf import DataToArray
+        >>> x = XanthosToNetcdf(xanthos_reference_file='<reference file>', xanthos_data_csv='<data file>')
+
+        # get output array
+        >>> x.data_array
+
+
     """
 
     # coordinate bounds for Xanthos
@@ -59,10 +90,75 @@ class XanthosToNetcdf:
     # xanthos default NoData value
     NODATA = np.nan
 
-    def __init__(self, xanthos_reference_file, xanthos_output_csv):
+    # primary key for data frames
+    KEY = 'grid_id'
+
+    # target xanthos reference columns to load and column names
+    REF_X_FIELD = 'longitude_index'
+    REF_Y_FIELD = 'latitude_index'
+
+    REF_COLUMNS = [KEY, REF_Y_FIELD, REF_X_FIELD]
+
+    # id field to rename from the input data
+    DATA_ID_FIELD = 'id'
+
+    def __init__(self, xanthos_reference_file, xanthos_data_csv):
 
         self._reference_file = xanthos_reference_file
-        self._output_csv = xanthos_output_csv
+        self._data_csv = xanthos_data_csv
+
+    @staticmethod
+    def check_exist(file_path):
+        """Ensure the file exists.
+
+        :param file_path:                   Full path with file name and extension to the input file
+        :type file_path:                    str
+
+        :return:                            Valid file path
+
+        """
+        if os.path.isfile(file_path):
+            return file_path
+
+        else:
+            raise IOError(f"USAGE:  File path '{file_path} cannot be located.")
+
+    @property
+    def reference_file(self):
+        """Validate reference file existence."""
+
+        return self.check_exist(self._reference_file)
+
+    @property
+    def data_csv(self):
+        """Validate reference file existence."""
+
+        return self.check_exist(self._data_csv)
+
+    @property
+    def df_reference(self):
+        """Load reference file into a data frame."""
+
+        # read in data csv
+        return pd.read_csv(self.reference_file, usecols=self.REF_COLUMNS)
+
+    @property
+    def df_data(self):
+        """Load data file into a data frame."""
+
+        # read in data csv
+        df = pd.read_csv(self.data_csv)
+
+        # rename 'id' column to 'grid_id' to assist join as key
+        df.rename(columns={self.DATA_ID_FIELD: self.KEY}, inplace=True)
+
+        return df
+
+    @property
+    def df_merge(self):
+        """Create a data frame that represents merged reference data and xanthos values from the input."""
+
+        return self.df_data.merge(self.df_reference, on='grid_id')
 
     @property
     def grid_array(self):
@@ -79,7 +175,41 @@ class XanthosToNetcdf:
         # build two-dimensional array matching the shape of the coordinates
         coords = np.zeros(shape=(y_coords.shape[0], x_coords.shape[0]))
 
-        # make entire array NaN
+        # make entire array set to NoData value
         coords[:] = self.NODATA
 
         return coords
+
+    @property
+    def data_array(self):
+        """Create a multi-dimensional array to house Xanthos data for each land cell in the coordinate plane.
+
+        :returns:                           3D array of land cell values per time step as they exist on the global
+                                            coordinate plane.  Shape:  (n_time, y, x)
+
+        """
+
+        # get years from data frame
+        df = self.df_data.copy()
+
+        # remove id column so only years or months are left
+        df.drop(columns=self.KEY, inplace=True)
+
+        # build coordinate y, x index list
+        coordinate_indices = [self.df_merge[self.REF_Y_FIELD].values, self.df_merge[self.REF_X_FIELD].values]
+
+        # construct initial 3D array of (n_time, y, x)
+        arr = np.empty(shape=(df.shape[1], self.grid_array.shape[0], self.grid_array.shape[1]))
+
+        for index, col in enumerate(df.columns):
+
+            # copy to preserve original
+            grid_array = self.grid_array.copy()
+
+            # data from the data frame for each time step based on the index locations in the global coordinate plane
+            grid_array[coordinate_indices] = df[col]
+
+            # add as a time dimension to the output 3D array
+            arr[index, :, :] = grid_array
+
+        return arr
